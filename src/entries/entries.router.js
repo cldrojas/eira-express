b6041a062cd4520055dffbad5ba6479272860d77
cldrojas <cld.rojasz@gmail.com>
2022-01-28 14:16:54 -0300

    add async call to service

--- src/entries/entries.router.js
+++ src/entries/entries.router.js
@@ -4,40 +4,51 @@ const Service = require('./entries.service');
 const router = express.Router();
 const service = new Service();
 
-router.get('/', (req, res) => {
-  const { category } = req.query;
-  const entries = service.getAll(category);
-
-  res.status(200).json(entries);
+router.get('/', async (req, res) => {
+  try {
+    const { category } = req.query;
+    const entries = await service.getAll(category);
+    res.status(200).json(entries);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
 });
 
-router.get('/:id', (req, res) => {
-  const { id } = req.params;
-  const entry = service.getById(parseInt(id));
-
-  entry
-    ? res.status(200).json(entry)
-    : res.status(404).json({ message: 'Entry not found' });
+router.get('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const entry = await service.getById(parseInt(id));
+    res.status(200).json(entry);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
 });
 
-router.post('/', (req, res) => {
-  const Entry = service.create(req.body);
-
-  res.status(201).json(Entry);
+router.post('/', async (req, res) => {
+  // TODO: get error handling on service
+  const entry = await service.create(req.body);
+  res.status(201).json(entry);
 });
 
-router.patch('/:id', (req, res) => {
-  const { id } = req.params;
-  const body = req.body;
-  const Entry = service.update(parseInt(id), body);
-  res.status(200).json(Entry);
+router.patch('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const body = req.body;
+    const entry = await service.update(parseInt(id), body);
+    res.status(200).json(entry);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
 });
 
-router.delete('/:id', (req, res) => {
-  const { id } = req.params;
-
-  const deleted = service.delete(parseInt(id));
-  res.status(200).json(deleted);
+router.delete('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleted = await service.delete(parseInt(id));
+    res.status(200).json(deleted);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
 });
 
 module.exports = router;
