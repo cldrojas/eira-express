d824fba2d749084db86b4a1cea865156d81e04eb
cldrojas <cld.rojasz@gmail.com>
2022-01-31 13:45:26 -0300

    update error handle

--- src/middlewares/validate.js
+++ src/middlewares/validate.js
@@ -4,11 +4,8 @@ function validate(dto, prop) {
   return (req, res, next) => {
     const data = req[prop];
     const { error } = dto.validate(data, { abortEarly: false });
-    if (error) {
-      next(boom.badRequest(error));
-    }
 
-    next();
+    error ? next(boom.badRequest(error)) : next();
   };
 }
 
